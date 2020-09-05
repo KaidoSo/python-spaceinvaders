5a6ea531ff97ce045a999b6742ef80a56f760dc6
KaidoSo <kaido.soomlais@europaskolan.nu>
2020-09-05 09:23:29 +0200

    added player movement

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('spaceship.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
 # Game Loop
@@ -32,5 +33,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        # if key is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
