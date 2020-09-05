98b8963793c4bffb3cf35b9ab5365c1c7e0b8988
kaidoso <kaido.soomlais@gmail.com>
2020-09-05 10:15:54 +0200

    added enemy Y movement when reaching screen end

--- main.py
+++ main.py
@@ -17,13 +17,15 @@ playerImg = pygame.image.load('spaceship.png')
 playerX = 370
 playerY = 480
 playerX_change = 0
+playerY_change = 40
+
 
 # Enemy
-
 enemyImg = pygame.image.load('invader.png')
 enemyX = random.randint(0, 800)
 enemyY = random.randint(50, 150)
-enemyX_change = 0
+enemyX_change = 0.3
+enemyY_change = 40
 
 
 def player(x, y):
@@ -45,12 +47,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        # if key is pressed check whether its right or left
+        # if key is pressed check whether its right or left move player that direction
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 playerX_change = -0.3
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.3
+        # if key is released the player stops
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -58,12 +61,23 @@ while running:
     # 5 = 5 + 0.1 -> 5 = 5 - 0.1
     # 5 = 5 + 0.1
 
+    # checking for boundaries of spaceship
     playerX += playerX_change
 
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
+
+    # Enemy movement
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
