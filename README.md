588a310a34a6d0fa3df2c35a6e00ac945de85dcb
kaidoso <kaido.soomlais@gmail.com>
2020-09-08 18:00:35 +0200

    adjusted bullet path to be independent from ship movement

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<a href='https://pngtree.com/so/space-background'>space-background png from pngtree.com</a>
+Background from <a href='https://pngtree.com/so/space-background'>space-background png from pngtree.com</a>
 
 <div> Icons made by <a href="https://www.flaticon.com/authors/xnimrodx" title="xnimrodx">xnimrodx</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></div>
 
--- main.py
+++ main.py
@@ -73,7 +73,9 @@ while running:
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
             if event.key == pygame.K_SPACE:
-                fire_bullet(playerX, bulletY)
+                if bullet_state is "ready":
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
         # if key is released the player stops
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -101,8 +103,12 @@ while running:
         enemyY += enemyY_change
 
     # Bullet movement
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+
     if bullet_state is "fire":
-        fire_bullet(playerX, bulletY)
+        fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
     player(playerX, playerY)
