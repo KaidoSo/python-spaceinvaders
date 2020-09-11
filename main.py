6a28d5a85a54c8c633966ebf9cfe744b4def8619
kaidoso <kaido.soomlais@gmail.com>
2020-09-11 16:42:09 +0200

    Added score

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ enemyX = []
 enemyY = []
 enemyX_change = []
 enemyY_change = []
-num_of_enemies = 6
+num_of_enemies = 10
 
 for i in range(num_of_enemies):
     enemyImg.append(pygame.image.load('invader.png'))
@@ -48,7 +48,18 @@ bulletX_change = 0
 bulletY_change = 10
 bullet_state = "ready"
 
-score = 0
+
+# Score
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+textX = 10
+textY = 10
+
+
+def show_score(x, y):
+    score = font.render("Score: " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
 
 
 def player(x, y):
@@ -127,8 +138,7 @@ while running:
         if collision:
             bulletY = 480
             bullet_state = "ready"
-            score += 1
-            print(score)
+            score_value += 1
             enemyX[i] = random.randint(0, 735)
             enemyY[i] = random.randint(50, 150)
 
@@ -144,4 +154,5 @@ while running:
         bulletY -= bulletY_change
 
     player(playerX, playerY)
+    show_score(textX, textY)
     pygame.display.update()
